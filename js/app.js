a21aef69206950d37957fc672b29e0023fd6e143
ronaalf <alfaro.ronald2@gmail.com>
2021-07-13 20:02:48 -0600

    add project

---
+++ js/app.js
@@ -0,0 +1,22 @@
+'use strict';
+const toggleBtn = document.querySelector(".sidebar-toggle");
+const closeBtn = document.querySelector(".close-btn");
+const sideBar = document.querySelector(".sidebar");
+
+toggleBtn.addEventListener("click", function () {
+    console.log(sideBar.classList);
+
+    // ONE WAY
+    // if (sideBar.classList.contains("show-sidebar")) {
+    //     sideBar.classList.remove("show-sidebar");
+    // } else {
+    //     sideBar.classList.add("show-sidebar");
+    // }
+
+    // THE OTHER WAY
+    sideBar.classList.toggle("show-sidebar");
+});
+
+closeBtn.addEventListener("click", function () {
+    sideBar.classList.remove("show-sidebar");
+});
